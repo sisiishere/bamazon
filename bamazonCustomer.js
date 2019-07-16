817153d071f6ac91e16ab3b603d9e74517eaac60
sisiishere <set38246@marymount.edu>
2019-07-16 16:15:40 -0400

    modify

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,31 +15,115 @@ var connection = mysql.createConnection({
 connection.connect(function(err)  {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
+    runSearch();
 });
+  
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What department id do you prefer?",
+        choices: [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "1":
+          item_id();
+          break;
+  
+        case "2":
+          item_id();
+          break;
+  
+        case "3":
+          item_id();
+          break;
+  
+        case "4":
+          item_id();
+          break;
+  
+        case "5":
+          item_id();
+          break;
 
-function afterConnection() {
-    connection.query("SELECT * FROM bamazon1", function(err, res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
-    })
-};
+        case "6":
+        item_id();
+        break;
 
-var inquirer = function() {
-    // if the length of the team array is 5 or higher, no more questions will be asked
-      inquirer.prompt([
-        {
-          item_id: "ID #",
-          message: "What is the item's ID?: ",
-          message: "Quantity of the product: ",
-          validate: function(bamazon1_stock) {
-            if (quantity >= bamazon1_stock) {
-              true;
-              console.log("Good to go! Buy 'em!");
-            }
-            else {
-            false;
-            console.log("Insufficiency. Decrease the amount.")
-            }
-          }}])}
+        case "7":
+        item_id();
+        break;
+
+        case "8":
+        item_id();
+        break;
+        
+        case "9":
+        item_id();
+        break;
+
+        case "10":
+        item_id();
+
+        }
+      });
+  }
+  
+  function item_id() {
+    inquirer
+      .prompt({
+        name: "product",
+        type: "input",
+        message: "What product are you searching for?"
+      })
+      .then(function(answer) {
+        var query = "SELECT id, product, department FROM bamazon1 WHERE ?";
+        connection.query(query, { prod: answer.prod }, function(err, res) {
+          if (err) throw err;
+          for (var i = 0; i < res.length; i++) {
+            console.log("id: " + res[i].item_id + " || Product: " + res[i].prod + " || Department: " + res[i].dep);
+          }
+          runSearch();
+        });
+      });
+  }
+  
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "quantity",
+        type: "input",
+        message: "How many quantities would you like to buy per item?"
+      })
+      .then(function(answer) {
+        var query = "SELECT stock, price FROM bamazon1 WHERE ?";
+        connection.query(query, function(err, res) {
+          if (answer <= stock) {
+          for (var i = 0; i < res.length; i++) {
+            console.log("Your order has been placed! Have a swell day!");
+          }}
+          else {
+              for (var i = 0; i < res.length; i++) {  
+                  console.log("The ultimate INSUFFICIENCY of it all!")
+              }
+          }
+          runSearch();
+        });
+      });
+  }
+  
+  
+  
